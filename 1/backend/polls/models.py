122903c8ac3e65315feaace171a90cf0ec7b8927
skaikru0518 <dante0518@outlook.com>
2025-02-11 18:33:01 +0100

    first django page - questions

--- 1/backend/polls/models.py
+++ 1/backend/polls/models.py
@@ -8,3 +8,19 @@ from django.utils.timezone import now
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
     publish_date = models.DateTimeField(default=now)
+
+    def __str__(self):
+        return self.question_text
+    
+
+
+class Choice(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE) # ha törlünk egy kerdest akkor a hozza kapcsolodo valasz is torlodni fog (erre jo a cascade)
+    choice_text = models.CharField(max_length=200)
+    vote = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.choice_text+" ("+self.question.question_text+")" # a kerdes kiirasa a valasztasi lehetoseg utan
+    
+    
+
--- 1/backend/polls/views.py
+++ 1/backend/polls/views.py
@@ -1,6 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from .models import Question, Choice
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello!")
+    all_questions = Question.objects.all().order_by("-publish_date") # select * from question order by publish_date desc
+    context = {"all_questions":all_questions}
+    return render(request, "index.html", context)
+
+def detail(request, question_id):
+    current_question = Question.objects.get(pk=question_id) # select * from question where id = question_id
+    context = {"question":current_question}
+    return render(request, "detail.html", context)
