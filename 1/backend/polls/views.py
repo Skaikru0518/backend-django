140e55bb851e431c1e012384418225ba2eae20ab
skaikru0518 <dante0518@outlook.com>
2025-02-25 17:24:54 +0100

    update

--- 1/backend/polls/views.py
+++ 1/backend/polls/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import Http404, HttpResponse
 from .models import Question, Choice
+
 # Create your views here.
 def index(request):
     all_questions = Question.objects.all().order_by("-publish_date") # select * from question order by publish_date desc
@@ -8,6 +9,9 @@ def index(request):
     return render(request, "index.html", context)
 
 def detail(request, question_id):
-    current_question = Question.objects.get(pk=question_id) # select * from question where id = question_id
+    try:
+        current_question = Question.objects.get(pk=question_id) # select * from question where id = question_id
+    except Question.DoesNotExist:
+        raise Http404("Question Does Not Exist!")
     context = {"question":current_question}
     return render(request, "detail.html", context)
